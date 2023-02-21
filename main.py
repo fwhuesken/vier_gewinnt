b670bff6b4813a1c1e4542b02b657c38d1301d98
Felix Huesken <fwhuesken@gmail.com>
2023-02-21 13:33:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+player1 = "Player 1"
+player2 = "Player 2"
+
+"""
+rowCount = 6
+columnCount = 7 # max 9
+board = []
+
+def createBoard(rowCount, columnCount):
+  baseRow = [""]
+  row = [""]
+  for i in range(columnCount):
+    row.append("") # creates row with number of columns
+    baseRow.append(i+1) # creates numbered row at the bottom
+  for i in range(rowCount):
+    board.append(row) # appends rows to board
+  board.append(baseRow) # appends numbered row at the bottom
+  #print(board)
+  return(board)
+
+board = createBoard(rowCount, columnCount)
+"""
+
+board = [['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', 1, 2, 3, 4, 5, 6, 7]]
+
+def prettyPrint(board):
+  print() 
+  for row in board:
+    for item in row:  
+     print(f"{item:^1}", end=" | ")
+    print("\n")
+  print()
+
+player = player1
+symbol = "X"
+
+def addMoveToBoard(nextMove, board, symbol):
+  #row = rowCount
+  board[5][nextMove] = symbol
+  #print(board)
+
+while True:
+  os.system("clear")
+  prettyPrint(board)
+  nextMove = int(input(f"{player}, enter a column number > "))
+  
+  addMoveToBoard(nextMove, board, symbol)
+  
+  if player == player1: 
+    player = player2
+    symbol = "O" # specify symbol on board for player1
+  else:
+    player = player1 # switch players for next round
+    symbol = "X" # specify symbol on board for player1
